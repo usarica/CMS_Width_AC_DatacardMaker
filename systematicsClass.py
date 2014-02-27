74f9d1007663cd2f405e07828c6b1199c599e169
Giacomo Ortona <gortona@cern.ch>
2014-02-27 15:55:49 +0100

    proper treatment of AsymPow systematics

--- systematicsClass.py
+++ systematicsClass.py
@@ -825,9 +825,8 @@ class systematicsClass:
         theFile.write("CMS_zz4l_mu param 0.93  -0.24/+0.26 \n")
         #theFile.write("CMS_zz4l_mu param 0.935929  -0.244308/+0.296613 \n") #stat + syst
         theFile.write("CMS_zz4l_kbkg param 1.0  0.10 \n")
-        theFile.write("CMS_zz4l_scale_syst shape1 1 1 - - \n")
-        #theFile.write("CMS_zz4l_APscale_syst param 0.0 1 [-3,3] \n")
-
+        theFile.write("CMS_zz4l_scale_syst shape1 - 1 - - \n")
+        theFile.write("CMS_zz4l_APscale_syst param 0.0 1 [-3,3] \n")
         
         #if( self.channel == self.ID_4mu):
 
