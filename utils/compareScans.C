a27619ca07207252ff58e05f62299edca10dc17a
Giacomo Ortona <gortona@cern.ch>
2014-03-10 18:02:48 +0100

    update

--- utils/compareScans.C
+++ utils/compareScans.C
@@ -1,21 +1,41 @@
 void compareScans(){
-
-  const int nfiles = 4;
+  gStyle->SetOptTitle(0);
+  const int nfiles = 6;
+  //2D fits
   //TString files[]={"cards_03_05_Unblind_093_2D/HCG/220/","cards_03_05_Unblind_2D/HCG/220/","cards_03_05_Unblind_093_2D/HCG/220/","cards_03_05_Unblind_2D/HCG/220_noSyst/","cards_03_05_Unblind_2D/HCG/220/"}
-  //TString files[]={"cards_03_05_Unblind_093_1DDgg/HCG/220/","cards_03_05_Unblind_1DDgg/HCG/220/","cards_03_05_Unblind_093_1DDgg/HCG/220/","cards_03_05_Unblind_1DDgg/HCG/220_noSyst/","cards_03_05_Unblind_1DDgg/HCG/220/"}
-  TString files[]={"cards_1DDgg_093_NoCostant/HCG/220/","cards_1DDgg_093_NoCostant/HCG/220/","cards_1DDgg_093_NoCostant/HCG/220/","cards_1DDgg_093_NoCostant/HCG/220_noSyst/","cards_03_05_Unblind_1DDgg/HCG/220/"}
-  //TString files[]={"cards_lowRScanObs_Dgg_up2/HCG/220_all/","cards_lowRScanObs_Dgg_up2/HCG/220_2e2mu/","cards_lowRScanObs_Dgg_up2/HCG/220_4e/","cards_lowRScanObs_Dgg_up2/HCG/220_4mu/","cards_03_05_Unblind_Dgg/HCG/220/"};
-  int colors[]={kBlack,kBlue,kRed+1,kGreen+2,kYellow+3,kBlack,kBlue,kRed+1,kGreen+2};
-  TString grnames[]={"Observed","Expected #mu=#mu_{exp}","Expected #mu=#mu_{obs}","Expected #mu=1 w/o syst","Observed #mu=1"};
-  //TString grnames[]={"all","2e2mu","4e","4mu"};
-  bool obs[] = {1,0,0,0,1,0,1};
+  //1D(m4l) fits
+  //TString files[]={"cards_03_05_Unblind_093_1Dm4l/HCG/220/","cards_03_05_Unblind_1Dm4l/HCG/220/","cards_03_05_Unblind_093_1Dm4l/HCG/220/","cards_03_05_Unblind_1Dm4l/HCG/220_noSyst/","cards_03_05_Unblind_1Dm4l/HCG/220/"}
+  //1D(Dgg)
+  //TString files[]={"cards_03_09_bkgFix_093_1DDgg/HCG/220/","cards_03_09_bkgFix_1_1DDgg/HCG/220/","cards_03_09_bkgFix_093_1DDgg/HCG/220/","cards_03_09_bkgFix_1_1DDgg/HCG/220_noSyst/"};
+  //Alternative K hyp.
+  TString files[]={"cards_03_05_Unblind_093_2D/HCG/220/","cards_03_05_Unblind_093_2D/HCG/220_noSyst/","cards_093_AlternativeKBKG/HCG/220/","cards_093_AlternativeKBKG/HCG/220_noSyst/","cards_093_AlternativeKBKG_28/HCG/220/","cards_093_AlternativeKBKG_28/HCG/220_noSyst/"};
+  //low r scans
+  //TString files[]={"cards_lowRScan_mu1_2D/HCG/220_all/","cards_lowRScan_mu1_2D/HCG/220_2e2mu/","cards_lowRScan_mu1_2D/HCG/220_4e/","cards_lowRScan_mu1_2D/HCG/220_4mu/","cards_lowRScanObs_2D/HCG/220_all/"}
+
+  //int colors[]={kBlack,kRed+1,kBlue,kGreen+2,kYellow+2,kYellow+2,kYellow+3,kBlack,kBlue,kRed+1,kGreen+2};
+  int colors[]={kBlack,kBlack,kYellow+2,kYellow+2,kPink-1,kPink-1};
+
+  //fit plots
+  //TString grnames[]={"Observed","Expected #mu=#mu_{exp}","Expected #mu=#mu_{obs}","Expected #mu=1 w/o syst","Observed #mu=1"};
+  //channels plots
+  //TString grnames[]={"Observed #mu=1","Observed #mu=1, 2e2#mu","Observed #mu=1 4e","Observed #mu=1 4#mu","Observed #mu=0.93"};
+  //Alternative K plots
+  TString grnames[]={"Observed #mu=#mu_{obs}","Expected #mu=#mu_{obs}","Observed #mu=#mu_{obs} K=1/2.8","Expected #mu=#mu_{obs} K=1/2.8","Observed #mu=#mu_{obs} K=2.8","Expected #mu=#mu_{obs} K=2.8"};
+
+  //tell this flag which are obsered
+  bool obs[] = {1,0,0,0,1,0,0};
   int mass = 220;
   int maxwidth = 30.0;
   bool blind = true;
+  bool uncBand = false;
+
 
   //values for 1DDgg_093, expected mu=0.93
-  double limits95[]={9.30681,12.6934,19.5321,31.2537,42.5569};
-  double limits68[]={3.33046,4.82275,9.2807,18.4322,27.625};
+  double limits95[]={6.1548, 7.98996, 12.193, 19.1693, 26.382};
+  double limits68[]={2.3321, 3.17787, 5.65613, 10.973, 16.4882x};
+  //values for 2D_093, expected mu=0.93
+  //double limits95[]={ 5.48667, 7.1966,10.8518,17.2703,24.0398};
+  //double limits68[]={2.07673,2.81889,4.97083,9.94989, 14.86};
 
   // gROOT->ProcessLine(".x tdrstyle.cc");
   gStyle->SetPadLeftMargin(0.16);
@@ -55,7 +75,7 @@ void compareScans(){
     gr0->SetName(grnames[i].Data());
     gr0->SetLineWidth(2.5);
     gr0->SetLineColor(colors[i]);
-    //if(!obs[i])gr0->SetLineStyle(2);
+    if(!obs[i])gr0->SetLineStyle(2);
     gr0->SetTitle(grnames[i].Data());
     leg->AddEntry(gr0);
     g[i]=(TGraph*)gr0->Clone();
@@ -71,7 +91,7 @@ void compareScans(){
     }
     double a =  (y[ipol+1]-y[ipol])/(x[ipol+1]-x[ipol]);
     double b = y[ipol]-a*x[ipol];
-    printf("%d) limit %.2f\n",i,(3.84-b)/a);
+    printf("%d) limit %.5f\n",i,(3.84-b)/a);
   }
 
 
@@ -93,35 +113,35 @@ void compareScans(){
   g[0]->GetYaxis()->SetRangeUser(0.,12.);//12
   g[0]->GetXaxis()->SetRangeUser(0.,gglimit);
 
-
-  TLine *l2_95=new TLine();
-  //l2_95->SetLineStyle(9);
-  l2_95->SetLineWidth(10);
-  l2_95->SetLineColor(kYellow);
-  l2_95->DrawLine(limits95[0],3.84,TMath::Min(limits95[4],gglimit),3.84);
-  l2_95->Draw();
-
-  TLine *l2_68=new TLine();
-  //l2_68->SetLineStyle(9);
-  l2_68->SetLineWidth(10);
-  l2_68->SetLineColor(kGreen);
-  l2_68->DrawLine(limits95[1],3.84,limits95[3],3.84);
-  l2_68->Draw("same");
-
-  TLine *l1_95=new TLine();
-  //l1_95->SetLineStyle(9);
-  l1_95->SetLineWidth(10);
-  l1_95->SetLineColor(kYellow);
-  l1_95->DrawLine(limits68[0],1,TMath::Min(limits68[4],gglimit),1);
-  l1_95->Draw("same");
-
-  TLine *l1_68=new TLine();
-  //l1_68->SetLineStyle(9);
-  l1_68->SetLineWidth(10);
-  l1_68->SetLineColor(kGreen);
-  l1_68->DrawLine(limits68[1],1,limits68[3],1);
-  l1_68->Draw("same");
-
+  if(uncBand){
+    TLine *l2_95=new TLine();
+    //l2_95->SetLineStyle(9);
+    l2_95->SetLineWidth(10);
+    l2_95->SetLineColor(kYellow);
+    l2_95->DrawLine(limits95[0],3.84,TMath::Min(limits95[4],gglimit),3.84);
+    if(uncBand)l2_95->Draw();
+    
+    TLine *l2_68=new TLine();
+    //l2_68->SetLineStyle(9);
+    l2_68->SetLineWidth(10);
+    l2_68->SetLineColor(kGreen);
+    l2_68->DrawLine(limits95[1],3.84,limits95[3],3.84);
+    if(uncBand)l2_68->Draw("same");
+    
+    TLine *l1_95=new TLine();
+    //l1_95->SetLineStyle(9);
+    l1_95->SetLineWidth(10);
+    l1_95->SetLineColor(kYellow);
+    l1_95->DrawLine(limits68[0],1,TMath::Min(limits68[4],gglimit),1);
+    if(uncBand)l1_95->Draw("same");
+    
+    TLine *l1_68=new TLine();
+    //l1_68->SetLineStyle(9);
+    l1_68->SetLineWidth(10);
+    l1_68->SetLineColor(kGreen);
+    l1_68->DrawLine(limits68[1],1,limits68[3],1);
+    if(uncBand)l1_68->Draw("same");
+  }
 
   TPaveText *pt = new TPaveText(0.1577181,0.9562937,0.9580537,0.9947552,"brNDC");
   pt->SetBorderSize(0);
@@ -166,20 +186,20 @@ void compareScans(){
   for(int i=0;i<nfiles;i++)g[i]->Draw("LSAME");
   leg->Draw("SAME");
 
-  TGraph *medians = new TGraph(2);
-  medians->SetPoint(0,limits95[2],3.84);
-  medians->SetPoint(1,limits68[2],1.0);
-  medians->SetFillStyle(0);
-  medians->SetMarkerStyle(30);
-  medians->SetMarkerColor(kBlue);
-  medians->Draw("PSAME");  
+  if(uncBand){
+    TGraph *medians = new TGraph(2);
+    medians->SetPoint(0,limits95[2],3.84);
+    medians->SetPoint(1,limits68[2],1.0);
+    medians->SetFillStyle(0);
+    medians->SetMarkerStyle(30);
+    //medians->SetMarkerSize(2);
+    medians->SetMarkerColor(kBlue);
+    medians->Draw("PSAME");  
+  }
 
-  //c1->SaveAs("can_scan1D_ggsm.C");
-  //c1->SaveAs("can_scan1D_ggsm.root");
-  //c1->SaveAs("can_scan1D_ggsm.eps");
-  c1->SaveAs("03_05_1DDggUncBand.gif");
-  c1->SaveAs("03_05_1DDggUncBand.eps");
-  c1->SaveAs("03_05_1DDggUncBand.pdf");
-  c1->SaveAs("03_05_1DDggUncBand.png");
+  c1->SaveAs("compareK_2.gif");
+  c1->SaveAs("compareK_2.eps");
+  c1->SaveAs("compareK_2.pdf");
+  c1->SaveAs("compareK_2.png");
 
 }
