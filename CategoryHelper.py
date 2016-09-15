7f204547fd8b33eb602bc61d5cec1592248f5fe6
Ulascan Sarica <ulascan90@gmail.com>
2016-09-15 10:32:33 -0400

    New input reader (almost done)

--- CategoryHelper.py
+++ CategoryHelper.py
@@ -29,31 +29,5 @@ class CategoryHelper:
          self.catNameList.append("")
       self.nCategories = len(self.catNameList)
 
-      #NO NEED FOR THESE, THE CONSTRUCTION OF ASYMQUAD AND VERTICALHISTINTERPOLATORPDF HANDLE EVERYTHING CONSISTENTLY
-      #self.CatUncScheme=0 # HARDCODED, TO BE DECIDED LATER ON
-      #self.systVar_catsplit_gg = []
-      #self.systVar_catsplit_VBF = []
-      #self.systVar_catsplit_qqzz = []
-      #self.systVar_catsplit_zjets = []
-
-      ## No need to fix these since nCategories==1 does not enter the loop (i.e. no systVars are created)!
-      #if(self.CatUncScheme==1): # Uncertainties on i->j contamination
-         #for icat in range(0,self.nCategories):
-            #for jcat in range(0,self.nCategories):
-               #if(jcat<=icat):
-                  #continue
-               #self.systVar_catsplit_gg.append(workspace.factory("cat_{0:.0f}in{1:.0f}_syst_ggzz[-3,3]".format(icat,jcat)))
-               #self.systVar_catsplit_VBF.append(workspace.factory("cat_{0:.0f}in{1:.0f}_syst_vbf_offshell[-3,3]".format(icat,jcat)))
-               #self.systVar_catsplit_qqzz.append(workspace.factory("cat_{0:.0f}in{1:.0f}_syst_qqzz[-3,3]".format(icat,jcat)))
-               #self.systVar_catsplit_zjets.append(workspace.factory("cat_{0:.0f}in{1:.0f}_syst_zjets[-3,3]".format(icat,jcat)))
-      #else: # Uncertainties on i->un-tagged contamination
-         #for icat in range(0,self.nCategories-1):
-            #self.systVar_catsplit_gg.append(workspace.factory("cat_{0:.0f}_syst_ggzz[-3,3]".format(icat)))
-            #self.systVar_catsplit_VBF.append(workspace.factory("cat_{0:.0f}_syst_vbf_offshell[-3,3]".format(icat)))
-            #self.systVar_catsplit_qqzz.append(workspace.factory("cat_{0:.0f}_syst_qqzz[-3,3]".format(icat)))
-            #self.systVar_catsplit_zjets.append(workspace.factory("cat_{0:.0f}_syst_zjets[-3,3]".format(icat)))
-
-
-
 
 
