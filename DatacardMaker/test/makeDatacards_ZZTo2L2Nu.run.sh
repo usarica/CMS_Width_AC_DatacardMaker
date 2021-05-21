aa55d51429ee17b66cf3aefdad580caec1b319cc
Ulascan Sarica <ulascan90@gmail.com>
2021-05-20 23:17:25 -0700

    Update scripts

--- DatacardMaker/test/makeDatacards_ZZTo2L2Nu.run.sh
+++ DatacardMaker/test/makeDatacards_ZZTo2L2Nu.run.sh
@@ -5,23 +5,31 @@ hypo=$2
 year=$3
 tpltag=$4
 dcinputstag=$5
-hasBoostedVH=$6
+datainputtag=$6
+hasBoostedVH=$7
 
 theCatScheme="nj012_2l2nu"
 theCatDir="CatScheme_Nj"
-theCats=( Nj_eq_0  Nj_eq_1 Nj_geq_2_pTmiss_lt_200 Nj_geq_2_pTmiss_ge_200 )
+theCats=( Nj_eq_0 Nj_eq_1 Nj_geq_2_pTmiss_lt_200 Nj_geq_2_pTmiss_ge_200 )
 if [[ $hasBoostedVH -eq 1 ]]; then
   theCatScheme="nj012_boostedhadvh_2l2nu"
   theCatDir="CatScheme_Nj_BoostedHadVH"
   theCats+=( BoostedHadVH)
 fi
+
 inputcards=/hadoop/cms/store/user/usarica/Offshell_2L2Nu/Worker/output/DatacardSpecs/${dcinputstag}/Offshell_inputs_13TeV_${year}/${theCatDir}/${hypo}
 tpldir=/hadoop/cms/store/user/usarica/Offshell_2L2Nu/Worker/output/Templates/${tpltag}/${theCatDir}/${hypo}/${year}
-outdirname=${date}_ZZ2L2Nu_InputTag_${dcinputstag}_TplTag_${tpltag}_${hypo}_${theCatScheme}
-
+datadir=/hadoop/cms/store/user/usarica/Offshell_2L2Nu/Worker/output/DCDataTrees_ZZTo2L2Nu/${datainputtag}/${theCatDir}/${hypo}
 if [[ "$(hostname)" == *"lxplus"* ]]; then
   inputcards=${inputcards//'/hadoop/cms/store/user/usarica/Offshell_2L2Nu/Worker/output/'}
   tpldir=${tpldir//'/hadoop/cms/store/user/usarica/Offshell_2L2Nu/Worker/output/'}
+  datadir=${tpldir//'/hadoop/cms/store/user/usarica/Offshell_2L2Nu/Worker/output/'}
+fi
+
+outdirname=${date}_ZZ2L2Nu_InputTag_${dcinputstag}_TplTag_${tpltag}_${hypo}_${theCatScheme}
+mLowVal=300
+if [[ "$tpltag" == "210508" ]] || [[ "$tpltag" == "210515" ]]; then
+  mLowVal=200
 fi
 
 for channel in 2e2nu 2mu2nu; do
@@ -39,9 +47,9 @@ for channel in 2e2nu 2mu2nu; do
     echo "Coordinates = ${theCoords}"
 
     if [[ "$hypo" == "SM" ]]; then
-      python makeWidthDatacards.py -b --writeoutput -i $inputcards -t $tpldir -a $outdirname --coord $theCoords --GHmodel 1 --CatScheme $theCatScheme --ac 0 --mLow 200 --mHigh 13000 --category $cat --channel $channel
+      python makeWidthDatacards.py -b --writeoutput -i $inputcards -t $tpldir -r $datadir -a $outdirname --coord $theCoords --GHmodel 1 --CatScheme $theCatScheme --ac 0 --mLow ${mLowVal} --mHigh 13000 --category $cat --channel $channel
     else
-      python makeWidthDatacards.py -b --writeoutput -i $inputcards -t $tpldir -a $outdirname --coord $theCoords --GHmodel 1 --CatScheme $theCatScheme --ac 2 --mLow 200 --mHigh 13000 --category $cat --channel $channel
+      python makeWidthDatacards.py -b --writeoutput -i $inputcards -t $tpldir -r $datadir -a $outdirname --coord $theCoords --GHmodel 1 --CatScheme $theCatScheme --ac 2 --mLow ${mLowVal} --mHigh 13000 --category $cat --channel $channel
     fi
 
     RUN_STATUS=$?
--- DatacardMaker/test/utils/buildCards.run.sh
+++ DatacardMaker/test/utils/buildCards.run.sh
@@ -7,25 +7,27 @@ opt=$4
 
 echo "Building $outname from $inname at sqrts=$sqrts and option $opt"
 
-if [[ "$opt" == "SM" ]];then
-   echo "SM case:"
-   echo "Command:" text2workspace.py \
-      -m 125 $inname -o $outname \
-      -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:multiSignalSpinZeroHiggs \
-      --PO="offshell" --PO="fai1fixed" --PO="sqrts="$sqrts
-   text2workspace.py \
-      -m 125 $inname -o $outname \
-      -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:multiSignalSpinZeroHiggs \
-      --PO="offshell" --PO="fai1fixed" --PO="sqrts="$sqrts
+
+runCmd="text2workspace.py -m 125 $inname -o $outname -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:multiSignalSpinZeroHiggs --PO=sqrts=${sqrts}"
+if [[ "$opt" == *"SM"* ]]; then
+  echo "Configuring for the SM case..."
+  runCmd="${runCmd} --PO=fai1fixed"
 else
-   echo "BSM case:"
-   echo "Command:" text2workspace.py \
-      -m 125 $inname -o $outname \
-      -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:multiSignalSpinZeroHiggs \
-      --PO="offshell" --PO="allowPMF" --PO="sqrts="$sqrts
-   text2workspace.py \
-      -m 125 $inname -o $outname \
-      -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:multiSignalSpinZeroHiggs \
-      --PO="offshell" --PO="allowPMF" --PO="sqrts="$sqrts
+  echo "Configuring for the BSM case..."
+  runCmd="${runCmd} --PO=allowPMF"
 fi
 
+if [[ "$opt" == *"OnshellOnly"* ]]; then
+  echo "Only on-shell, so not including the off-shell option..."
+else
+  echo "Including the off-shell option..."
+  runCmd="${runCmd} --PO=offshell"
+fi
+
+if [[ "$opt" == *"UseRVoverRF"* ]]; then
+  echo "Using (R,RV) instead of (RF, RV) for the parametrization..."
+  runCmd="${runCmd} --PO=uservoverrf"
+fi
+
+echo "Command: ${runCmd}"
+${runCmd}
